ce856bb61383b50bed2284e004a0ccde7c272b60
Francisco Farinha <fquico1999@gmail.com>
2020-01-25 10:31:03 -0800

    Changed robot model to turtlebot for asr and refined VoiceController

--- ros_ws/src/ros_test/src/voice_control.py
+++ ros_ws/src/ros_test/src/voice_control.py
@@ -9,10 +9,14 @@ class VoiceController():
 
         #Create the node and set shutdown command
         rospy.init_node('voice_control')
-        rospy.on_shutdown(self.shutdown)
+        #rospy.on_shutdown(self.shutdown) Seems to cause some issues when trying to kill node
 
         #Initialze output msg
         self.msg = Twist()
+
+        #Set speed
+        self.speed_linear = 0.2
+        self.speed_angular = 0.5
 
         #Initalize publisher
         self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=5)
@@ -23,19 +27,25 @@ class VoiceController():
     def parse_kws(self, kws_data):
 
         if kws_data.data.find('forward') > -1:
-            self.msg.linear.x = 0.3
+            self.msg.linear.x = self.speed_linear
             self.msg.angular.z = 0
         elif kws_data.data.find('back') > -1:
-            self.msg.linear.x = -0.3
+            self.msg.linear.x = -self.speed_linear
             self.msg.angular.z = 0
         elif kws_data.data.find('right') > -1:
-            self.msg.linear.x = 0.1
-            self.msg.angular.z = -1
+            self.msg.linear.x = 0
+            self.msg.angular.z = -self.speed_angular
         elif kws_data.data.find('left') > -1:
-            self.msg.linear.x = 0.1
-            self.msg.angular.z = 1
+            self.msg.linear.x = 0
+            self.msg.angular.z = self.speed_angular
         elif kws_data.data.find('stop') > -1:
             self.msg = Twist()
+        elif kws_data.data.find('full') > -1 and kws_data.data.find('speed')> -1:
+            self.speed_linear = 0.4
+            self.speed_angular = 1
+        elif kws_data.data.find('half') > -1 and kws_data.data.find('speed') > -1:
+            self.speed_linear = 0.2
+            self.speed_angular = 0.5
 
         self.pub.publish(self.msg)
 
